4ce06f3a78909d5f4de5494e892c7d993fc4e495
numoonchld <raghavendra.saralaya@gmail.com>
2019-02-10 19:01:45 +0530

    routing and material nav sys works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first-page', component: FirstPageComponent},
+  { path: 'second-page', component: SecondPageComponent},
+  { path: 'third-page', component: ThirdPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavSysComponent } from './components/nav-sys/nav-sys.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavSysComponent
+    NavSysComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent
   ],
   imports: [
     BrowserModule,
